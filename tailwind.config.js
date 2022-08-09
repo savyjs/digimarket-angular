48e100b0937c0a3cc39d982658e5553d53f5089f
ehsan <savvyversa@gmail.com>
2022-08-09 16:09:57 +0430

    Add tw-elements and flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      darkMode: true,
+      colors: {
+        transparent: 'transparent',
+        primary: '#EF4056',
+        secondary: '#008eb2',
+        error: '#f44336',
+        success: '#4caf50',
+        alert: '#ff9800',
+        dark: '#212121',
+        gray: colors.gray,
+        blue: colors.sky,
+        red: colors.rose,
+        pink: colors.fuchsia
+      },
+      fontFamily: {
+        sans: ['IRANSans']
+      }
+    }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+    require('tw-elements/dist/plugin')
+  ]
 }
