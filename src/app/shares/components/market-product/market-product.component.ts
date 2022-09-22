01e500723321039cfd86612b6942322e23562094
savvy <savvyversa@gmail.com>
2022-09-22 21:28:21 +0330

    feat: update color boxes

--- src/app/shares/components/market-product/market-product.component.ts
+++ src/app/shares/components/market-product/market-product.component.ts
@@ -20,4 +20,14 @@ export class MarketProductComponent implements OnInit {
     console.log('this.item:', this.item)
   }
 
+
+  pickTextColorBasedOnBgColorSimple(bgColor: string, lightColor = '#FFF', darkColor = '#000') {
+    var color = (bgColor.charAt(0) === '#') ? bgColor.substring(1, 7) : bgColor;
+    var r = parseInt(color.substring(0, 2), 16); // hexToR
+    var g = parseInt(color.substring(2, 4), 16); // hexToG
+    var b = parseInt(color.substring(4, 6), 16); // hexToB
+    return (((r * 0.299) + (g * 0.587) + (b * 0.114)) > 186) ?
+      darkColor : lightColor;
+  }
+
 }
